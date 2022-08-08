6469c334e63a2ddf78fa885059af7f8395146722
Nneka-x <nnenwanze16@gmail.com>
2022-08-08 20:47:15 +0200

    first commit

---
+++ coding.py
@@ -0,0 +1,5 @@
+num1 = int(input(100))
+num2 = int(input(59))
+diff = num1 - num2
+
+print(diff)
---
+++ first.py
@@ -0,0 +1,13 @@
+#print("Hello World")
+
+# x=5
+# y="hello world" 
+# print (x) 
+# print (y) 
+
+# for x in range(0, 101, 2):
+#  print(x) 
+
+
+
+print(type(int(hello world, base=10)))
