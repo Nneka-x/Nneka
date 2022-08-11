4b4190f36bf3522d14999f344aa6d5fbd24a2662
Nneka-x <102034200+Nneka-x@users.noreply.github.com>
2022-08-11 20:39:52 +0200

    Add files via upload

--- first.py
+++ first.py
@@ -8,6 +8,58 @@
 # for x in range(0, 101, 2):
 #  print(x) 
 
+# import random
+
+# print(random.randrange(1,10))
+
+# a = "mango","banana","cherry"
+# print(a[0])   
 
 
-print(type(int(hello world, base=10)))
+# a = "Nneka Nwanze"
+# print(len(a))
+
+# b  = "hello world"
+# print(b[2:5])
+
+# n = "thewisdom machine"
+# print(n[1:])
+
+txt = "the best things in life are free"
+print("free" in txt)
+
+
+# txt = "the best things in life are free"
+# if "free" in txt:
+#   print("yes,  why not")
+
+# b = "Thewisdom machine"
+# print(b[-5:-2])
+
+# a = "Thewisdom machine"
+# print(a.upper())
+
+# a = "Thewisdom machine"
+# print(a.lower())
+
+# a = " Thewisdom machine"
+# print(a.strip())   
+
+# a = "Hello, world"
+# print(a.replace("H" , "J"))
+
+# a = "Hello, World"
+# print(a.split(","))
+
+# a = "hello"
+# b = "World"
+# c = a + b
+# print(c)
+
+# a = "hello"
+# b = "World"
+# c = a + " " + b
+# print(c)
+
+# for x in "cherry":
+#   print (x)
